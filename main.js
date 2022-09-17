8ccd952477e26fca06a8448c20f12ad86d151aa0
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-16 22:39:58 -0500

    4

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 addEventListener("DOMContentLoaded",()=>{
-    let n1=0,  n2=0, n3=0, n4=0, r=0
-    n1=Number(prompt("Ingrese el numero para saber su media",3));
-    n2=Number(prompt("Ingrese el numero para saber su media",4));
-    n3=Number(prompt("Ingrese el numero para saber su media",5));
-    n4=Number(prompt("Ingrese el numero para saber su media",7));
-    r=(n1+n2+n3+n4)/4;
-    console.log(`La media es de: "${r}" `);
+    let n1=0, n2=0,m=0
+    n1=Number(prompt("Ingrese el primer valor para calcular el area de un triangulo",4));
+    n2=Number(prompt("Ingresa el segundo valor para calcular el area del triangulo: ",3));
+    m=(n1*n2)/2
+    console.log(`El area del triangulo es: ${m}`)
 })
