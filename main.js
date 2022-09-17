9209349c4c36c801f111a8db1631eb6b9f8ab0b1
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-16 22:55:29 -0500

    9

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 addEventListener("DOMContentLoaded",()=>{
-    let a=0,b=0,c=0
-    a=Number(prompt("Ingresa el primer valor: ",4))
-    b=Number(prompt("Ingrese el segundo el valor: ",2))
-    c=Number(prompt("Ingresa el tercer valor:  ",5))
-    let exp1=0,exp2=0,exp3=0
-    exp1=(a+7*c)/((b+2-a)+2*b)
-    console.log(`el valor de la primera expresion es: ${exp1}`)
+    let  valor=0,pi=3.14,r=0,h=0
+    valor=Number(prompt("Ingresa el primer valor"));
+    r=Number(prompt("Ingrese el radio del cilindro: "))
+    h=Number(prompt("Ingresa la altura: "))
+    let area=0,volumen=0
+    area=(valor*Math.PI*Math.pow(r,2))+((valor*pi*r)*h)
+    volumen=((pi*r**2)*h)
+
+    console.log(`El area del cilindro es de: ${area}`)
+    console.log(`El volumen del cilindro es de: ${volumen}`)
+
 })
