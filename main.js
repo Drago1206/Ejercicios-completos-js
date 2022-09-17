101e69e03a71e201ec3ea39f60ddec409f3f3a05
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-16 22:50:59 -0500

    8

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 addEventListener("DOMContentLoaded",()=>{
-    let radio=0, v=4/3
-    radio=Number(prompt("Ingresa el radio de la esfera",3));
-    let vesfera=(v*3.14*radio**2);
-    console.log(`la respuesta es:  ${vesfera}`);
+    let a=0,b=0,c=0
+    a=Number(prompt("Ingresa el primer valor: ",4))
+    b=Number(prompt("Ingrese el segundo el valor: ",2))
+    c=Number(prompt("Ingresa el tercer valor:  ",5))
+    let exp1=0,exp2=0,exp3=0
+    exp1=(a+7*c)/((b+2-a)+2*b)
+    console.log(`el valor de la primera expresion es: ${exp1}`)
 })
