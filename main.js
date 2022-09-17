b9c88b0fdaf62a38bb7f80677298a92b3920a509
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-17 01:32:32 -0500

    16

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 addEventListener("DOMContentLoaded",()=>{
-let c=0
-c=Number(prompt("Ingrese los grados celcius que desee convertir a farenheit:  ",35))
-let f=0
-f=(9/5)*c+32
-console.log(`La cantidad de grados celcius equivale a :${f} grados farenheit`)
+let c1=0,c2=0,c3=0
+c1=Number(prompt("Ingrese la primera calificacion: "))
+c2=Number(prompt("Ingrese la segunda calificacion: "))
+c3=Number(prompt("Ingrese la tercera calificacion: "))
+let prom=0,exam=0,Trabajo=0
+prom=(c1+c2+c3)/3
+exam=Number(prompt("Ingrese la nota del examen: "))
+Trabajo=Number(prompt("Ingrese la calificacion del trabajo final: "))
+
+let final=0
+final=(prom+exam+Trabajo)
+
+console.log(`El promedio final de la materia del algoritmos es de: ${final}`)
+
 
 
 })
