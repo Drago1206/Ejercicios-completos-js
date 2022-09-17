82491fb9d5f75fdde5c31fe92e8087bc91d8b7db
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-17 01:06:59 -0500

    15

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 addEventListener("DOMContentLoaded",()=>{
-    let P_or=0,Des=0.10
-    p_or=Number(prompt("Ingrese el valor del medicamento: "))
-    let= Vproduct=0,Vfinal=0
-    Vproduct=(P_or*Des)
-    Vfinal=(P_or-Vproduct)
-    console.log(`El valor orginial del producto es de: ${Vproduct}`)
-    console.log(`El valor final con descuento es de: ${Vfinal}`)
+let c=0
+c=Number(prompt("Ingrese los grados celcius que desee convertir a farenheit:  ",35))
+let f=0
+f=(9/5)*c+32
+console.log(`La cantidad de grados celcius equivale a :${f} grados farenheit`)
 
 
 })
