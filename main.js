caf0d12ee9b79c2be043ed9b03d5530c3a27877e
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-16 22:56:29 -0500

    10

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 addEventListener("DOMContentLoaded",()=>{
-    let  valor=0,pi=3.14,r=0,h=0
-    valor=Number(prompt("Ingresa el primer valor"));
-    r=Number(prompt("Ingrese el radio del cilindro: "))
-    h=Number(prompt("Ingresa la altura: "))
-    let area=0,volumen=0
-    area=(valor*Math.PI*Math.pow(r,2))+((valor*pi*r)*h)
-    volumen=((pi*r**2)*h)
-
-    console.log(`El area del cilindro es de: ${area}`)
-    console.log(`El volumen del cilindro es de: ${volumen}`)
-
+    let a=0,b=0,c=0,x=24,rta=0
+    a=Number(prompt("Ingresa el valor de a: "))
+    b=Number(prompt("Ingresa el segundo valor: "))
+    c=Number(prompt("Ingrese el valor de c: "))
+    rta=(a*Math.pow(x,2)+b*x+c)
+    console.log(`el resultado de la raiz es:${rta}` )
 })
