6c75458812edcc90be49a016c6a2e2fa59825d58
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-09-18 15:50:24 -0500

    18

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 addEventListener("DOMContentLoaded",()=>{
-let p1=0,p2=0,p3=0
-p1=Number(prompt("Ingrese el valor del producto"))
-p2=Number(prompt("Ingrese el valor del producto"))
-p3=Number(prompt("Ingrese el valor del producto"))
+let Hr=0,Pg=0
+Hr=Number(prompt("Ingrese las horas laboradas: "))
+Pg=Number(prompt("Ingrese su sueldo"))
 
-let sub=0,iva=0,Tl=0
-sub=(p1+p2+p3)
-iva=(sub*0.19)
-Tl=(sub+iva)
-console.log(`El subtotal es de: ${sub}`)
-console.log(`El valor con el iva  es de: ${iva}`)
-console.log(`El valor total es de: ${Tl}`)
-
+if(Hr>48){
+    extras=Hr-48
+    let sueldo=(48*Pg)
+    let extra=sueldo+(Hr*(Pg+Pg*0.5))
+    console.log(`El sueldo por 48 horas es de: ${Pg}`)
+    console.log(`Las horas extras trabajadas son de${Hr}`)
+    console.log(`El sueldo con la horas extras es de: ${extras}`)
+    console.log(`El sueldo es de: ${sueldo}`)
+}else{
+    let Sueldo=Hr*Pg
+    console.log("El sueldo por ",Hr,"es de: ",Sueldo)
+}
 
 })
