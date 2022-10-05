c3f1db17e308258dc2b552bccbff93f3b13dc050
JhoanSebastianGelvesHernándezADSO <Jhoangelves1205@gmail.com>
2022-10-05 16:04:11 -0500

    21

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 addEventListener("DOMContentLoaded",()=>{
-let A=0,B=0,C=0,Sueldo=0
-A=Number(prompt("Ingresa la primera venta: "))
-B=Number(prompt("Ingresa la segunda venta: "))
-C=Number(prompt("Ingresa la tercera venta: "))
-Sueldo=Number(prompt("Ingresa el sueldo"))
-
-let comision=(A+B+C)*0.10
-let total=(Sueldo+comision)
-
-console.log(`El sueldo del trabajador es de:  ${Sueldo}`)
-console.log(`La comision del mes por las tres ventas es de:  ${comision}`)
-console.log(`El sueldo ${total}`)
+      // 21. Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución,
+    // por favor utilizar funciones matemáticas de Python para ello
+    // d = rc((x2-x1)^2 + (y2-y1)^2)
+document.write(`21. Procesos para calcular la distancia entre dos puntos...<br>`)
+let rc=0,x1=0,x2=0,y1=0,y2=0,D=0
+rc=Number(prompt("Ingrese el valor de rc"))
+x1=Number(prompt("Ingrese el valor de x1"))
+x2=Number(prompt("Ingrese el valor de x2"))
+y1=Number(prompt("Ingrese el valor de y1"))
+y2=Number(prompt("Ingrese el valor de y2"))
+D=rc*((x2-x1)^2 + (y2-y1)^2)
+document.write(`La distancia entre los dos puntos es de : ${D}`)
 })
